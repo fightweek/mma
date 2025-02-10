c281aeac53d4c1114dc8b6f2ea5f92f5cb9dff22
JINHYUNTAEK <jht2512@naver.com>
2025-02-10 11:36:49 -0800

    empty commit

--- src/main/java/my/mma/InitializeFightersAndEvents.java
+++ src/main/java/my/mma/InitializeFightersAndEvents.java
@@ -1,14 +1,15 @@
 package my.mma;
 
 import lombok.extern.slf4j.Slf4j;
+import my.mma.event.entity.FightEvent;
+import my.mma.event.entity.FighterFightEvent;
+import my.mma.event.repository.FightEventRepository;
 import my.mma.fighter.dto.CrawlerDto;
 import my.mma.fighter.dto.CrawlerDto.EventCrawlerDto;
 import my.mma.fighter.dto.CrawlerDto.FighterCrawlerDto;
-import my.mma.event.entity.FightEvent;
 import my.mma.fighter.entity.Fighter;
-import my.mma.event.entity.FighterFightEvent;
-import my.mma.event.repository.FightEventRepository;
 import my.mma.fighter.repository.FighterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
@@ -22,18 +23,17 @@ import java.util.List;
 import java.util.Optional;
 
 @Slf4j
-//@Component
+@Component
 @Transactional(readOnly = true)
 public class InitializeFightersAndEvents {
-
-    @Value("${python.uri}")
-    private String pythonURI;
 
     private final FighterRepository fighterRepository;
     private final FightEventRepository fightEventRepository;
     private final WebClient webClient;
 
     public InitializeFightersAndEvents(FighterRepository fighterRepository, FightEventRepository fightEventRepository) {
+        String pythonURI = "http://localhost:5000";
+        log.info("Python Server URI: {}", pythonURI);
         this.fighterRepository = fighterRepository;
         this.fightEventRepository = fightEventRepository;
         this.webClient = WebClient.builder()
@@ -49,6 +49,7 @@ public class InitializeFightersAndEvents {
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(CrawlerDto.class);
+        System.out.println("============");
         responseMono.flatMap(crawlerDto -> {
             Mono<Void> saveFighters = Mono.fromRunnable(() -> {
                 List<FighterCrawlerDto> fighterDtos = crawlerDto.getFighters();
@@ -60,7 +61,7 @@ public class InitializeFightersAndEvents {
                     }
                 }
             });
-
+            System.out.println("===============");
             Mono<Void> saveEvents = Mono.fromRunnable(() -> {
                 List<EventCrawlerDto> eventDtos = crawlerDto.getEvents();
                 for (EventCrawlerDto eventDto : eventDtos) {
@@ -82,7 +83,6 @@ public class InitializeFightersAndEvents {
                     fightEventRepository.save(fightEvent);
                 }
             });
-
             return saveFighters.then(saveEvents); // Fighter 저장 후 Event 저장
         }).subscribe();
     }
