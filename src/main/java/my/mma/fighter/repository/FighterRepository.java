a04ca233f46ae1fbb9e8d20778e1bdcaad051943
JINHYUNTAEK <jht2512@naver.com>
2025-11-12 01:51:52 -0800

    feat: unittest-fighterrepository use h2 in-memory-db for repository test

--- src/main/java/my/mma/fighter/repository/FighterRepository.java
+++ src/main/java/my/mma/fighter/repository/FighterRepository.java
@@ -5,14 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface FighterRepository extends JpaRepository<Fighter, Long> {
-    @Query("SELECT f FROM Fighter f WHERE LOWER(REPLACE(f.name, ' ', '')) = LOWER(REPLACE(:name, ' ', ''))")
-    Optional<Fighter> findByName(@Param("name") String name);
+    Optional<Fighter> findByName(String name);
 
     Optional<Page<Fighter>> findByNameContainingIgnoreCase(String name, Pageable pageable);
 
