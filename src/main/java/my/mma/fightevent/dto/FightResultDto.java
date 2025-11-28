935772842b04a391bbdb347aea95d2d9a0ffc6b2
JINHYUNTAEK <jht2512@naver.com>
2025-11-28 14:58:54 -0800

    fix: fightresult endtime (HH:MM -> MM:SS)

--- src/main/java/my/mma/fightevent/dto/FightResultDto.java
+++ src/main/java/my/mma/fightevent/dto/FightResultDto.java
@@ -4,13 +4,15 @@ import lombok.*;
 import my.mma.fightevent.entity.property.FightResult;
 import my.mma.fightevent.entity.property.WinMethod;
 
+import java.time.format.DateTimeFormatter;
+
 @Builder
 public record FightResultDto(WinMethod winMethod, int round, String endTime, String description,
                              boolean draw, boolean nc) {
 
     public static FightResultDto toDto(FightResult result){
         return FightResultDto.builder()
-                .endTime(result.getEndTime().toString())
+                .endTime(result.getEndTime().format(DateTimeFormatter.ofPattern("HH:mm:ss")))
                 .round(result.getRound())
                 .winMethod(result.getWinMethod())
                 .description(result.getWinDescription())
